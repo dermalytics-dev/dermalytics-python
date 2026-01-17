9ad74b5d2827e7142f80ac947199d6f47ee02ed2
Dermalytics <support@dermalytics.dev>
2026-01-17 23:15:02 +0100

    Bump version to 0.1.2

--- dermalytics/__init__.py
+++ dermalytics/__init__.py
@@ -10,7 +10,7 @@ from .exceptions import (
     ValidationError,
 )
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = [
     "Dermalytics",
     "DermalyticsError",
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dermalytics",
-    version="0.1.1",
+    version="0.1.2",
     author="Dermalytics",
     author_email="support@dermalytics.dev",
     description="Python SDK for the Dermalytics API - Skincare Ingredient Analysis",
